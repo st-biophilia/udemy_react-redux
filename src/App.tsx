8b5f3e7810044ac611feb828f8306eda39861be5
阿達大輝 <taiki@adachinoMacBook-puro.local>
2022-01-09 16:20:11 +0900

    components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
 
-function App() {
-  return (
-    <React.Fragment>
-      <h1>おはよう</h1>
-    </React.Fragment>
-  );
+/**
+ * class component
+ */
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>おはよう</h1>
+        <input type='text' onChange={() => console.log('hi')}></input>
+        <Cat />
+        <Cat />
+        <Cat />
+        <Cat />
+      </React.Fragment>
+    );
+  }
 }
+class Cat extends React.Component {
+  render() {
+    return <div>meow</div>
+  }
+}
+
+/** 
+ * functional component
+ */ 
+// const App = () => {
+//   return (
+//     <React.Fragment>
+//       <h1>おはよう</h1>
+//       <input type='text' onChange={() => console.log('hi')}></input>
+//       <Cat />
+//       <Cat />
+//       <Cat />
+//       <Cat />
+//     </React.Fragment>
+//   );
+// }
+
+// const Cat = () => {
+//   return <div>meow</div>
+// }
 
 export default App;

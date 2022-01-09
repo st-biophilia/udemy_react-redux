b8936b83251b8bb79ac6ef69464c213b2d93375f
阿達大輝 <taiki@adachinoMacBook-puro.local>
2022-01-09 16:46:03 +0900

    learn props

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,30 @@
 import React from 'react';
 
-/**
- * class component
- */
-class App extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <h1>おはよう</h1>
-        <input type='text' onChange={() => console.log('hi')}></input>
-        <Cat />
-        <Cat />
-        <Cat />
-        <Cat />
-      </React.Fragment>
-    );
-  }
-}
-class Cat extends React.Component {
-  render() {
-    return <div>meow</div>
-  }
+type props = {
+  name: string,
+  age: number
 }
 
-/** 
- * functional component
- */ 
-// const App = () => {
-//   return (
-//     <React.Fragment>
-//       <h1>おはよう</h1>
-//       <input type='text' onChange={() => console.log('hi')}></input>
-//       <Cat />
-//       <Cat />
-//       <Cat />
-//       <Cat />
-//     </React.Fragment>
-//   );
-// }
-// const Cat = () => {
-//   return <div>meow</div>
-// }
-
+const App = () => {
+  const profiles = [
+    {name:'taro', age:10},
+    {name:'hanako', age:5},
+    {name:'taiki', age:22}
+  ]
+  return (
+    <React.Fragment>
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index}/>
+      })}
+    </React.Fragment>
+  );
+}
+const User: React.FC<props> = (props) => {
+  return (
+    <React.Fragment>
+      <div>私は {props.name} です。年齢は {props.age}歳です</div>
+    </React.Fragment>
+  )
+}
+  
 export default App;
